9a236cd066e6b6ab8a5d85f7907bf213978af2cc
Raonny Moreira <raonnyti@gmail.com>
2026-02-19 16:09:01 -0300

    objetos

---
+++ objetos.js
@@ -0,0 +1,27 @@
+const pessoa = {
+    nome: 'Raonny',
+    sobrenome: 'Souza',
+    idade: '36',
+    temCNH: true
+}
+
+const autor = {
+    nome: 'J.R.R. Tolkien',
+    idade: 98
+}
+
+const livro = {
+    titulo: 'O Hobbit',
+    paginas: 310,
+    ...autor //spread Operator
+} 
+
+livro.publicado = true;
+livro.idiomas = 'Inglês, Português';
+
+//console.log(livro);
+
+delete livro.paginas;
+
+
+console.log(livro);
